fdfecbf10ae6a43ee8d56d408044ffd1054d7546
siwolsmu89 <siwol_smuire89@naver.com>
2020-07-07 08:22:49 +0900

    2020/07/07 Repository created

---
+++ c9board_v1/documents/SQL files/[1] Creating DB and User.sql
@@ -0,0 +1,5 @@
+-- Should be connected and executed by ROOT user.
+
+CREATE DATABASE coders9_board_v1 DEFAULT CHARACTER SET UTF8 COLLATE utf8_general_ci;
+
+GRANT ALL PRIVILEGES ON coders9_board_v1.* TO siwolsmu@'localhost' IDENTIFIED BY 'zxcv1234';
