9be262385b4d37e49eaa3cb58dbf9b33693711ab
siwolsmu89 <siwol_smuire89@naver.com>
2020-07-07 09:09:11 +0900

    2020/07/07 created ConnectionUtil.java

---
+++ c9board_v1/src/v1/board/c9/utils/ConnectionUtil.java
@@ -0,0 +1,26 @@
+package v1.board.c9.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+
+	static {
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public Connection getConnection() throws SQLException {
+		String url = "jdbc:mariadb://localhost:3306/coders9_board_v1?useUnicode=true&characterEncoding=utf8";
+		String user = "siwolsmu";
+		String pwd = "zxcv1234";
+		
+		return DriverManager.getConnection(url, user, pwd);
+	}
+	
+}
